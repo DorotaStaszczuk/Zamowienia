e7026bbc849b2fca72e0261a3c994789f965c39b
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-23 20:01:29 +0100

    address model added

--- Z_app/models.py
+++ Z_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+USER_TYPE_CHOICES = (
+    (0, 'Sprzedawca'),
+    (1, 'Klient'),
+    )
+
 class Product(models.Model):
     name = models.CharField(max_length=64, verbose_name="Nazwa")
     manufacturer = models.CharField(max_length=64, verbose_name="Producent")
@@ -16,3 +21,14 @@ class Order(models.Model):
     payment_deadline = models.DateTimeField(verbose_name="Termin_platnosci")
     total_price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Sumaryczna cena")
     products = models.ManyToManyField(Product)
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, choices=USER_TYPE_CHOICES, verbose_name="Użytkownik")
+    company_name = models.CharField(max_length=90, blank=True, verbose_name="Nazwa firmy")
+    name = models.CharField(max_length=64, verbose_name="Imię")
+    surname= models.CharField(max_length=64, verbose_name="Nazwisko")
+    street = models.CharField(max_length=350, verbose_name="Ulica")
+    house_number = models.IntegerField(verbose_name="Numer domu")
+    apartment_number = models.PositiveSmallIntegerField(verbose_name="Numer lokalu")
+    zip_code = models.CharField(max_length=6, verbose_name="Kod pocztowy")
+    city = models.CharField(max_length=350, verbose_name="Miejscowość")
