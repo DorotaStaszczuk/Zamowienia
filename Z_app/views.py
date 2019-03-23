e308ce5f8366921330cf9e2b8d897851991d9bb2
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-23 18:16:29 +0100

    main site view added

--- Z_app/views.py
+++ Z_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Product
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class MainSiteView(ListView):
+    model = Product
+    paginate_by = 50
+
+    def get(self, request):
+        ctx = {'products': Product.objects.all()}
+        return render(request, 'main.html', ctx)
