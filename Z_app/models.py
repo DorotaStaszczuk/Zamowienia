76cd882f1aca1fa2f9245bb99285d45030924fbc
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-23 18:12:31 +0100

    product model added

--- Z_app/models.py
+++ Z_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=64, verbose_name="Nazwa")
+    manufacturer = models.CharField(max_length=64, verbose_name="Producent")
+    description = models.TextField(verbose_name="Opis")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Cena")
+    image = models.ImageField(null=True, blank=True, verbose_name="Zdjęcie")
