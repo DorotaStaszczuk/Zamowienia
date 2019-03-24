20b73a2a9d94ecccc87d94c0ac115bc333eb0165
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-24 11:49:30 +0100

    main site view and product view added

--- Z_app/views.py
+++ Z_app/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic.detail import DetailView
 from .models import Product
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 
 class MainSiteView(ListView):
@@ -12,3 +15,6 @@ class MainSiteView(ListView):
     def get(self, request):
         ctx = {'products': Product.objects.all()}
         return render(request, 'main.html', ctx)
+
+class ProductView(DetailView):
+    model = Product
