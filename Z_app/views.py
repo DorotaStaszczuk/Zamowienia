00f5b21c006feb97e21df9970ce9ffa7a93ceb62
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-24 11:57:21 +0100

    add product view added

--- Z_app/views.py
+++ Z_app/views.py
@@ -5,8 +5,7 @@ from django.views.generic.detail import DetailView
 from .models import Product
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-
+from django.urls import reverse
 
 class MainSiteView(ListView):
     model = Product
@@ -18,3 +17,16 @@ class MainSiteView(ListView):
 
 class ProductView(DetailView):
     model = Product
+
+class AddProductView(CreateView):
+    model = Product
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('product', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return redirect(self.get_success_url())
