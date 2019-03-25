e9443d4f4836da3acccd4d3b728026020765c1f7
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-03-25 14:26:54 +0100

    login and myuser forms added

---
+++ Z_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from Z_app.models import User, Product
+
+
+class LoginForm(forms.Form):
+    login = forms.CharField(max_length=64)
+    password = forms.CharField(max_length=64, widget=forms.PasswordInput, label="Hasło")
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
